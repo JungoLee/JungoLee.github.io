886400048fd85a379f11af706b4333ee5f7047c9
Jungo <Jungo>
2021-06-22 17:50:03 +0900

    모바일 추억따위 더조은컴퓨터 아카데미에서 ... 다 버려버려... 제이쿼리 모바일 테마따뤼 안된다규.

--- view/Playing/02_hovers/js/script.js
+++ view/Playing/02_hovers/js/script.js
@@ -1,10 +1,10 @@
-﻿jQuery(document).ready(function(){
+﻿$(document).ready(function(){
 	var windowH=$(window).height();
-		
+
 	$('body').height(windowH);
 	$('body').height(windowH);
 
-	
+
 		$('html,body').on('mousemove',function(event){
 			posX=event.pageX;
 			posY=event.pageY;
@@ -14,28 +14,28 @@
 				var thisL=$(this).offset().left;
 				var thisW=$(this).width();
 				var thisH=$(this).height();
-				var cover=$(this).find('.hover_cover');			
-				
+				var cover=$(this).find('.hover_cover');
+
 				cover.css({top:0,left:posX-thisL-thisW/2});
-				
+
 				if (posY>thisT && posY<thisT+thisH && posX>thisL && posX<thisL+thisW) {
 					cover.css({top:0,left:0});
 				}
-		
+
 
 
 				$(this).click(function(){
 					cover.animate({top:0,left:0},1000000000);
-				});				
+				});
 			});
-				
-		});	
-		
 
-	
+		});
 
 
 
-	
+
+
+
+
 
 });
--- view/Project/before/13_mobile/1limit/index/js/script3.js
+++
@@ -1,8 +0,0 @@
-﻿$(function(){
-	$('.typeMoney').click(function(){
-		$('.typing').stop().animate({bottom:0},500);
-	});
-	$('.typing').click(function(){
-		$('.typing').stop().animate({bottom:'-100%'},500);
-	});	
-});
