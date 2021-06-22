bb34b6e79bd8431fe83a4a1fca1cca2d290e0e31
Jungo <Jungo>
2021-06-22 17:30:35 +0900

    경로수정

--- view/Project/before/13_mobile/1limit/index/js/script1.js
+++ view/Project/before/13_mobile/1limit/index/js/script1.js
@@ -19,17 +19,17 @@
 	$('#page3 .popup2 .no').click(function(){
 		$('.popup').fadeOut();
 	});
-		
-	
+
+
 	$('#page4>.header>.center>a').click(function(){
 		var tg=$(this);
 		var i=tg.index();
 		var content=$('#page4>.content>.content_hidden>div');
-	
-		var index=tg.attr('data-index');
-		
 
-		
+		var index=tg.attr('data-index');
+
+
+
 		$('#page4>.header>.center>a').removeClass('on');
 		tg.addClass('on');
 		if (i==i) {
@@ -55,86 +55,86 @@
 			$('#page4 .content3>.line>.w15').css({width:0}).stop().animate({width:'15%'},1200);
 			$('#page4 .content3>.line>.w11').css({width:0}).stop().animate({width:'11%'},1200);
 			$('#page4 .content3>.line>.w8').css({width:0}).stop().animate({width:'8%'},1200);
-			$('#page4 .content3>.line>.w2').css({width:0}).stop().animate({width:'2%'},1200);			
+			$('#page4 .content3>.line>.w2').css({width:0}).stop().animate({width:'2%'},1200);
 		};
 		return false;
 	});
-	
 
-	
+
+
 	///////////////////////////////////////////////
-	
+
 	///////////// 그래프 만들기
-	
+
 	////////////////////////////////////////////////
 
-	
-	
+
+
 	function setTimeoutId1(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS1++
-		perText.eq(0).text(countS1+'%'); 
-		
+		perText.eq(0).text(countS1+'%');
+
 		if (countS1>51) return ;
 
 		setTimeoutId1()
 	},20)
-	}; 
+	};
 	function setTimeoutId2(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS2++
-		perText.eq(1).text(countS2+'%'); 
-		
+		perText.eq(1).text(countS2+'%');
+
 		if (countS2>15) return ;
 
 		setTimeoutId2()
 	},120)
-	}; 
+	};
 	function setTimeoutId3(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS3++
-		perText.eq(2).text(countS3+'%'); 
-		
+		perText.eq(2).text(countS3+'%');
+
 		if (countS3>14) return ;
 
 		setTimeoutId3()
 	},150)
-	}; 
+	};
 	function setTimeoutId4(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS4++
-		perText.eq(3).text(countS4+'%'); 
-		
+		perText.eq(3).text(countS4+'%');
+
 		if (countS4>10) return ;
 
 		setTimeoutId4()
 	},200)
-	}; 
+	};
 	function setTimeoutId5(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS5++
-		perText.eq(4).text(countS5+'%'); 
-		
+		perText.eq(4).text(countS5+'%');
+
 		if (countS5>7) return ;
 
 		setTimeoutId5()
 	},200)
-	}; 
+	};
 	function setTimeoutId6(){
 	setTimeout(function(){
 		var perText=$('#page4 .content3>.persent>li');
 		countS6++
-		perText.eq(5).text(countS6+'%'); 
-		
+		perText.eq(5).text(countS6+'%');
+
 		if (countS6>1) return ;
 
 		setTimeoutId6()
 	},200)
-	}; 	
+	};
 
 });
