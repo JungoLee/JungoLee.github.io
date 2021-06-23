840ded4de3ce823b191f21d7ea055ebb7ef9f398
Jungo <Jungo>
2021-06-23 14:25:49 +0900

    땅걷기 만들기

--- 3d/js/script.js
+++ 3d/js/script.js
@@ -1,114 +1,279 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-const animate = function () {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-};
-
-animate();
 
 
-import * as THREE from '../build/three.module.js';
+			let camera, scene, renderer, controls;
 
-import Stats from './jsm/libs/stats.module.js';
+			const objects = [];
 
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
-import { RoomEnvironment } from './jsm/environments/RoomEnvironment.js';
+			let raycaster;
 
-import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from './jsm/loaders/DRACOLoader.js';
+			let moveForward = false;
+			let moveBackward = false;
+			let moveLeft = false;
+			let moveRight = false;
+			let canJump = false;
 
-let mixer;
+			let prevTime = performance.now();
+			const velocity = new THREE.Vector3();
+			const direction = new THREE.Vector3();
+			const vertex = new THREE.Vector3();
+			const color = new THREE.Color();
 
-const clock = new THREE.Clock();
-const container = document.getElementById( 'container' );
+			init();
+			animate();
 
-const stats = new Stats();
-container.appendChild( stats.dom );
+			function init() {
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.outputEncoding = THREE.sRGBEncoding;
-container.appendChild( renderer.domElement );
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+				camera.position.y = 10;
 
-const pmremGenerator = new THREE.PMREMGenerator( renderer );
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xffffff );
+				scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xbfe3dd );
-scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+				const light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+				light.position.set( 0.5, 1, 0.75 );
+				scene.add( light );
 
-const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
-camera.position.set( 5, 2, 8 );
+				controls = new PointerLockControls( camera, document.body );
 
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 0.5, 0 );
-controls.update();
-controls.enablePan = false;
-controls.enableDamping = true;
+				const blocker = document.getElementById( 'blocker' );
+				const instructions = document.getElementById( 'instructions' );
 
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( 'js/libs/draco/gltf/' );
+				instructions.addEventListener( 'click', function () {
 
-const loader = new GLTFLoader();
-loader.setDRACOLoader( dracoLoader );
-loader.load( 'models/gltf/LittlestTokyo.glb', function ( gltf ) {
+					controls.lock();
 
-    const model = gltf.scene;
-    model.position.set( 1, 1, 0 );
-    model.scale.set( 0.01, 0.01, 0.01 );
-    scene.add( model );
+				} );
 
-    mixer = new THREE.AnimationMixer( model );
-    mixer.clipAction( gltf.animations[ 0 ] ).play();
+				controls.addEventListener( 'lock', function () {
 
-    animate();
+					instructions.style.display = 'none';
+					blocker.style.display = 'none';
 
-}, undefined, function ( e ) {
+				} );
 
-    console.error( e );
+				controls.addEventListener( 'unlock', function () {
 
-} );
+					blocker.style.display = 'block';
+					instructions.style.display = '';
 
+				} );
 
-window.onresize = function () {
+				scene.add( controls.getObject() );
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+				const onKeyDown = function ( event ) {
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+					switch ( event.code ) {
 
-};
+						case 'ArrowUp':
+						case 'KeyW':
+							moveForward = true;
+							break;
 
+						case 'ArrowLeft':
+						case 'KeyA':
+							moveLeft = true;
+							break;
 
-function animate() {
+						case 'ArrowDown':
+						case 'KeyS':
+							moveBackward = true;
+							break;
 
-    requestAnimationFrame( animate );
+						case 'ArrowRight':
+						case 'KeyD':
+							moveRight = true;
+							break;
 
-    const delta = clock.getDelta();
+						case 'Space':
+							if ( canJump === true ) velocity.y += 350;
+							canJump = false;
+							break;
 
-    mixer.update( delta );
+					}
 
-    controls.update();
+				};
 
-    stats.update();
+				const onKeyUp = function ( event ) {
 
-    renderer.render( scene, camera );
+					switch ( event.code ) {
 
-}
+						case 'ArrowUp':
+						case 'KeyW':
+							moveForward = false;
+							break;
+
+						case 'ArrowLeft':
+						case 'KeyA':
+							moveLeft = false;
+							break;
+
+						case 'ArrowDown':
+						case 'KeyS':
+							moveBackward = false;
+							break;
+
+						case 'ArrowRight':
+						case 'KeyD':
+							moveRight = false;
+							break;
+
+					}
+
+				};
+
+				document.addEventListener( 'keydown', onKeyDown );
+				document.addEventListener( 'keyup', onKeyUp );
+
+				raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+				// floor
+
+				let floorGeometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+				floorGeometry.rotateX( - Math.PI / 2 );
+
+				// vertex displacement
+
+				let position = floorGeometry.attributes.position;
+
+				for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+					vertex.fromBufferAttribute( position, i );
+
+					vertex.x += Math.random() * 20 - 10;
+					vertex.y += Math.random() * 2;
+					vertex.z += Math.random() * 20 - 10;
+
+					position.setXYZ( i, vertex.x, vertex.y, vertex.z );
+
+				}
+
+				floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+				position = floorGeometry.attributes.position;
+				const colorsFloor = [];
+
+				for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+					color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					colorsFloor.push( color.r, color.g, color.b );
+
+				}
+
+				floorGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsFloor, 3 ) );
+
+				const floorMaterial = new THREE.MeshBasicMaterial( { vertexColors: true } );
+
+				const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+				scene.add( floor );
+
+				// objects
+
+				const boxGeometry = new THREE.BoxGeometry( 20, 20, 20 ).toNonIndexed();
+
+				position = boxGeometry.attributes.position;
+				const colorsBox = [];
+
+				for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+					color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					colorsBox.push( color.r, color.g, color.b );
+
+				}
+
+				boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
+
+				for ( let i = 0; i < 500; i ++ ) {
+
+					const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
+					boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+
+					const box = new THREE.Mesh( boxGeometry, boxMaterial );
+					box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+					box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+					box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+
+					scene.add( box );
+					objects.push( box );
+
+				}
+
+				//
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				document.body.appendChild( renderer.domElement );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				const time = performance.now();
+
+				if ( controls.isLocked === true ) {
+
+					raycaster.ray.origin.copy( controls.getObject().position );
+					raycaster.ray.origin.y -= 10;
+
+					const intersections = raycaster.intersectObjects( objects );
+
+					const onObject = intersections.length > 0;
+
+					const delta = ( time - prevTime ) / 1000;
+
+					velocity.x -= velocity.x * 10.0 * delta;
+					velocity.z -= velocity.z * 10.0 * delta;
+
+					velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+					direction.z = Number( moveForward ) - Number( moveBackward );
+					direction.x = Number( moveRight ) - Number( moveLeft );
+					direction.normalize(); // this ensures consistent movements in all directions
+
+					if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+					if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+					if ( onObject === true ) {
+
+						velocity.y = Math.max( 0, velocity.y );
+						canJump = true;
+
+					}
+
+					controls.moveRight( - velocity.x * delta );
+					controls.moveForward( - velocity.z * delta );
+
+					controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+					if ( controls.getObject().position.y < 10 ) {
+
+						velocity.y = 0;
+						controls.getObject().position.y = 10;
+
+						canJump = true;
+
+					}
+
+				}
+
+				prevTime = time;
+
+				renderer.render( scene, camera );
+
+			}
