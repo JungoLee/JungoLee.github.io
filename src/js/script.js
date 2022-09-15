c876628c924d4c169568ad060d0a3ce3cc42a1d5
Lee Siwoo <jungo91@etribe.co.kr>
2022-09-15 09:49:40 +0900

    올리기

--- src/js/script.js
+++ src/js/script.js
@@ -175,7 +175,7 @@ jQuery(document).ready(function () {
         if(selectVal == "Show all"){
             $('.content').show();
             return false;
-        }else if(selectVal == "Kakao" || selectVal == "google_re" || selectVal == "3d Test"){
+        }else if(selectVal == "Kakao" || selectVal == "google_re" || selectVal == "3d Test" || selectVal == "glove_front"){
 
         }else{
             $('.content').hide();
